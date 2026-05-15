97e7a96baebac6cde7dde907e5f9618b12c17b81
felipegarces123 <felipegarces@gmail.com>
2026-05-15 16:56:08 -0300

    Add files via upload

---
+++ vite.config.ts
@@ -0,0 +1,17 @@
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import federation from '@module-federation/vite';
+
+export default defineConfig({
+  plugins: [
+    react(),
+    federation({
+      name: 'mfeApp',
+      filename: 'remoteEntry.js',
+      exposes: {
+        './App': './src/main/routes/index.tsx',
+      },
+      shared: ['react', 'react-dom', 'react-router-dom'],
+    }),
+  ],
+});
